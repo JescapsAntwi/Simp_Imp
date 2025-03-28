b4cd6511d8b2cf36576899ccaacc2fddd910acb5
JescapsAntwi <antwijescaps1@gmail.com>
2025-03-28 00:52:32 +0000

    Added two new files

--- src/bs_imp.java
+++ src/bs_imp.java
@@ -1,32 +1,41 @@
 public class bs_imp {
     public static void main(String[] args) {
-        int[] my_array = {34, 51, 32, 76, -1, 42, 30};
-
-        //binary search
-        public static int binarySearch(int[] arrays, int targetValue) {
-
-            //first value in the array
-            int left = 0;
-
-            //last value in the array
-            int right = arrays.length - 1;
-
-            while (left <= right) {
-                //compute index of middle value
-                int mid = (left + right) / 2;
-
-                if (arrays[mid] == targetValue) {
-                    return mid;
-                }
-                if (arrays[mid] < targetValue) {`
-                  left = mid + 1;
-                }
-                else {
-                    right = mid - 1;
-                }
-            }
-            return -1;
+        int[] my_array = {-1, 30, 32, 76, 100, 420, 30000};
+        //call binary search function
+        int my_target = 76;
+        int result = binarySearch(my_array, my_target);
+        if (result != -1) {
+            System.out.println("The value " + my_target + " was found at index " + result);
         }
+    }
+    //binary search
+    public static int binarySearch(int[] arrays, int targetValue) {
+
+        //first value in the array
+        int left = 0;
+
+        //last value in the array
+        int right = arrays.length - 1;
+
+        while (left <= right) {
+            //compute index of middle value
+            int mid = (left + right) / 2;
+
+            //middle value is target
+            if (arrays[mid] == targetValue) {
+                return mid;
+            }
+            //update index of first value
+            if (arrays[mid] < targetValue) {
+                left = mid + 1;
+            }
+            //update index of last value
+            else {
+                right = mid - 1;
+            }
+        }
+        //target value not found
+        return -1;
     }
 }
 
---
+++ src/bubble.java
@@ -0,0 +1,26 @@
+//bubble sort implementation
+import java.util.Arrays;
+public class bubble {
+    public static void main(String[] args) {
+        int[] my_array = {-1, 300, 2, 38, 91, 17, 43, -4, -98};
+        System.out.println("Sorted array " + Arrays.toString(bubbleSort(my_array)));
+    }
+
+    //bubble sort method
+    public static int[] bubbleSort(int[] arrays) {
+        //outer loop
+        for (int i=0; i<arrays.length - 1; i++ ){
+            for (int j =0; j <arrays.length - i - 1; j++){
+                if (arrays[j] > arrays[j + 1]) {
+                    //swap elements
+                    int value = arrays[j];
+                    arrays[j] = arrays[j+1];
+                    arrays[j + 1] = value;
+                }
+
+            }
+        }
+        return arrays;
+    }
+}
+
