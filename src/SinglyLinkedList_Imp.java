39f89b61ec84dd5d5d2d0f4c66a4b9e649bc4835
JescapsAntwi <antwijescaps1@gmail.com>
2025-04-18 11:11:18 +0000

    changes

--- src/SinglyLinkedList_Imp.java
+++ src/SinglyLinkedList_Imp.java
@@ -42,6 +42,14 @@ public class SinglyLinkedList_Imp {
                 head = head.next;
                 return;
             }
+            Node current = head;
+            while(current.next != null) {
+                if(current.next.data == data) {
+                    current.next = current.next.next;
+                    return;
+                }
+                current = current.next;
+            }
         }
     }
 }
