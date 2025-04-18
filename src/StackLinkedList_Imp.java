9d2b0b4f0a0f7cc0af5857b34270ed91b55ecdeb
JescapsAntwi <antwijescaps1@gmail.com>
2025-04-18 14:28:01 +0000

    Changes

--- src/StackLinkedList_Imp.java
+++ src/StackLinkedList_Imp.java
@@ -1,2 +1,30 @@
 public class StackLinkedList_Imp {
+    private Node top;
+
+    class Node{
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+        //push operation
+        public void push(int data){
+            Node newNode = new Node(data);
+            newNode.next = top;
+            top = newNode;
+        }
+
+        //Pop operation
+        public int pop(){
+            if(isEmpty()){
+                System.out.println("Stack is empty");
+                return -1;
+            }
+            int data = top.data;
+
+
+        }
+
+    }
 }
