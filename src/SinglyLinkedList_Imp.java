804d81fb64fe7a6537c64c871b432eea440d871b
JescapsAntwi <antwijescaps1@gmail.com>
2025-04-18 10:47:23 +0000

    Minor Changes

---
+++ src/SinglyLinkedList_Imp.java
@@ -0,0 +1,7 @@
+//Concept: Nodes where each node contains data and a pointer to the next node.
+public class SinglyLinkedList_Imp {
+    class Node {
+        int data;
+        Node next;
+    }
+}
