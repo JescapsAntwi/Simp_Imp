81f70bdd3679430fc4e2ad47d1f84fafbea3d91c
JescapsAntwi <antwijescaps1@gmail.com>
2025-04-18 13:49:38 +0000

    Changes

--- src/StackArrays_Imp.java
+++ src/StackArrays_Imp.java
@@ -1,2 +1,16 @@
+//Concept: LIFO Structure (Last In First Out Structure)
 public class StackArrays_Imp {
+    class Stack {
+        int maxsize;
+        int[] stackArray;
+        int top;
+
+        public Stack(int size){
+            maxsize = size;
+            top = -1;
+            stackArray = new int[maxsize];
+        }
+        //adding an element onto the stack
+
+    }
 }
