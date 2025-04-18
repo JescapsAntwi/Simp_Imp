d80480376355565ffe510659df1b133686b19d1f
JescapsAntwi <antwijescaps1@gmail.com>
2025-04-18 14:06:20 +0000

    Changes

--- src/StackArrays_Imp.java
+++ src/StackArrays_Imp.java
@@ -11,6 +11,36 @@ public class StackArrays_Imp {
             stackArray = new int[maxsize];
         }
         //adding an element onto the stack
+        public void push(int value) {
+            if(isFull()){
+                System.out.println("Stack is full");
+                return;
+            }
+            stackArray[++top] = value;
+        }
+        public int pop(){
+            if(isEmpty()) {
+                System.out.println("Stack is empty");
+                return -1;
+            }
+            return stackArray[top--]; //decrement top after removing
 
+        }
+        public int peek() {
+            if(isEmpty()){
+                System.out.println("Stack is empty");
+                return -1;
+            }
+            return stackArray[top];
+        }
+        public boolean isEmpty(){
+            return stackArray[top] == -1;
+        }
+
+        public boolean isFull() {
+            return (top == maxsize - 1); //check if the top element is equal to the last valid index of the array
+        }
     }
 }
+
+
