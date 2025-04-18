1787cbef980ee68cdd7b475d81ea90b51001ccdf
JescapsAntwi <antwijescaps1@gmail.com>
2025-04-18 12:35:52 +0000

    Changes

--- src/DoublyLinkedList_Imp.java
+++ src/DoublyLinkedList_Imp.java
@@ -1,2 +1,31 @@
 public class DoublyLinkedList_Imp {
+    class DNode {
+        int data;
+        DNode next;
+        DNode prev;
+
+        public DNode(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    class DoublyLinkedList {
+        DNode head;
+        DNode tail;
+
+        //Add node at the end
+        public void append(int data){
+            DNode newNode = new DNode(data);
+            if(head == null){
+                head = newNode;
+                tail = newNode;
+                return;
+            }
+            tail.next = newNode;
+        }
+
+    }
 }
+
