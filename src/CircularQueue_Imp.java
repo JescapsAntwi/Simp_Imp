8dde0f6d7dd0e657d89b67a9e730bfbbe93573e1
JescapsAntwi <antwijescaps1@gmail.com>
2025-04-19 18:08:41 +0000

    Minor Changes

--- src/CircularQueue_Imp.java
+++ src/CircularQueue_Imp.java
@@ -3,4 +3,11 @@ public class CircularQueue_Imp {
     private int[] queueArray;
     private int front;
     private int rear;
+
+    public CircularQueue_Imp(int size){
+        maxSize = size + 1;
+        queueArray = new int[maxSize];
+        front = 0;
+        rear = 0;
+    }
 }
