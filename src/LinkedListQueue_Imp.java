d3a087f26adca3e0d4018c3ce760f775c247f2fd
JescapsAntwi <antwijescaps1@gmail.com>
2025-04-19 17:35:55 +0000

    Minor Changes

--- src/LinkedListQueue_Imp.java
+++ src/LinkedListQueue_Imp.java
@@ -13,7 +13,13 @@ public class LinkedListQueue_Imp {
     //add at the end
     public void enqueue(int data){
         Node newNode = new Node(data);
-
+        if(isEmpty()){
+            first = newNode;
+        }
+        else {
+            last.next = newNode;
+        }
+        last = newNode;
     }
 }
 
