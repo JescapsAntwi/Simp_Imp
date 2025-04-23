669a2dc608b8c5c42aa913b7e170c378b540aac7
JescapsAntwi <antwijescaps1@gmail.com>
2025-04-23 19:29:41 +0000

    Minor Changes

--- src/Quicksort_Imp.java
+++ src/Quicksort_Imp.java
@@ -1,5 +1,6 @@
 //this is an implementation of the quicksort algorithm
 //test of how the algorithm works
 // int[] sample_array = {3, 1, 6, 9, 8, 4, 5, -1};
+// pivot = 3
 public class Quicksort_Imp {
 }
