4ce28e5123940360977c6e45d3d7594cc219c7fd
JescapsAntwi <antwijescaps1@gmail.com>
2025-04-28 12:58:19 +0000

    Minor Changes

--- src/LinearSearch_Imp.java
+++ src/LinearSearch_Imp.java
@@ -2,12 +2,12 @@ public class LinearSearch_Imp {
     public static void main(String[] args) {
 
         int[] test_array = {9, 6, -1, 67, 89, 900, 21, 6};
-        int result = LinearSearch(test_array, 800);
+        int target = 900;
+        int result = LinearSearch(test_array, target);
         if(result != -1){
-
         }
         //call LinearSearch function
-        System.out.println("The value was found at " + LinearSearch(test_array, 800));
+        System.out.println("The value was found " + target + " was found at index " + result);
     }
     //Linear Search Implementation
     public static int LinearSearch(int[] array, int targetValue){
@@ -20,5 +20,8 @@ public class LinearSearch_Imp {
         return -1;
     }
 }
+
+
+
 
 
