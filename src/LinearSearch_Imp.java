589f2657fa88c365dab31f4ca77addef6eed3f6f
JescapsAntwi <antwijescaps1@gmail.com>
2025-04-28 12:49:38 +0000

    Minor Changes

--- src/LinearSearch_Imp.java
+++ src/LinearSearch_Imp.java
@@ -2,5 +2,8 @@ public class LinearSearch_Imp {
     public static void main(String[] args) {
 
     }
-    //
+    //Linear Search Implementation
+    public static int LinearSearch(int[] array, int targetValue){
+
+    }
 }
