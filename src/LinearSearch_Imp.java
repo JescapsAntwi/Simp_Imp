4fa522da5605508fde6627de73b63a6e7a6eef01
JescapsAntwi <antwijescaps1@gmail.com>
2025-04-29 23:24:04 +0000

    Minor Changes

--- src/LinearSearch_Imp.java
+++ src/LinearSearch_Imp.java
@@ -20,6 +20,6 @@ public class LinearSearch_Imp {
         return -1;
     }
 }
-//Implement bubble sort
+//Implement bubble sort on the
 //Next up; sort using bubble sort and search for a value in the array using linear search
 
