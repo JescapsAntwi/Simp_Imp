2f8816ea08442aa0fe1d9fdd273a43c25500732f
JescapsAntwi <antwijescaps1@gmail.com>
2025-05-08 18:02:15 +0000

    Minor Changes

--- src/LinearSearch_Imp.java
+++ src/LinearSearch_Imp.java
@@ -16,7 +16,7 @@ public class LinearSearch_Imp {
                 return i;
             }
         }
-        //value is not in array so r
+        //value is not in array so re
         return -1;
     }
 }
