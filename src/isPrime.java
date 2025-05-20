7a396029c6e90bddb52c76a80cb080a4bed18dad
JescapsAntwi <antwijescaps1@gmail.com>
2025-05-20 20:54:43 +0000

    Minor Changes

--- src/isPrime.java
+++ src/isPrime.java
@@ -4,7 +4,7 @@ public class isPrime {
         System.out.println(checkPrime(5));
         System.out.println(checkPrime(-1));
         System.out.println(checkPrime(0));
-        //func
+        //funct
     }
     static boolean checkPrime(int n){
         if (n < 0 || n == 1){
