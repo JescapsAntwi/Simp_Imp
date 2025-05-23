3f9e95c70613db6b4c54341505d3e95e728929cb
JescapsAntwi <antwijescaps1@gmail.com>
2025-05-23 21:32:27 +0000

    Minor Changes

--- src/isPrime.java
+++ src/isPrime.java
@@ -4,20 +4,19 @@ public class isPrime {
         System.out.println(checkPrime(5));
         System.out.println(checkPrime(-1));
         System.out.println(checkPrime(0));
-        //function will be call
+        //function will be cal
     }
-    static boolean checkPrime(int n){
-        if (n < 0 || n == 1){
+        static boolean checkPrime ( int n){
+            if (n < 0 || n == 1) {
+                return false;
+            } else if (n > 1) {
+                for (int i = 2; i < n; i++) {
+                    if (n % i == 0) {
+                        return false;
+                    }
+                }
+                return true;
+            }
             return false;
         }
-        else if (n > 1){
-            for (int i = 2; i <n; i++){
-                if (n % i == 0){
-                    return false;
-                }
-            }
-            return true;
-        }
-        return false;
     }
-}
